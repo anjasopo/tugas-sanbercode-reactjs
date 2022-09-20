c863306d535f563106e97fe07c27e8a0b4a89a35
anjasopo <anjasopo@gmail.com>
2022-09-20 20:39:18 +0700

    Tugas 7

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="article">
+        <h1>Hi, Saya Anjas</h1>
+      </div>
+      <p>Saya dari Batch 38 sudah berhasil menginstall React</p>
+    </div>
+  );
+}
+
+export default App;
