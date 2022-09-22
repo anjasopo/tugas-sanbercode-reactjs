6ac8c0597bba0eacec0e8d55074d2f22257cd27a
anjasopo <anjasopo@gmail.com>
2022-09-22 13:41:36 +0700

    Tugas 9

--- tugas-reactjs/src/App.js
+++ tugas-reactjs/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import Tugas7 from './Tugas7/tugas7';
 import Tugas8 from './Tugas8/tugas8';
+import Tugas9 from './Tugas9/tugas9';
 
 const App = () => {
   return (
     <>
       <Tugas7/>
       <Tugas8 name="Anjas Susetya" batch="38" email="anjasopo@gmail.com"/>
+      <Tugas9/>
     </>
   );
 }
---
+++ tugas-reactjs/src/Tugas9/tugas9.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+import "../style.css"
+
+const Tugas9 = () => {
+
+    let [angka, setCount] = useState(1)
+    const angkaHandle = () => {
+        setCount(angka += 1)
+    }
+
+    return (
+    <div className="Box">
+        <p style={{ textAlign: "center" }}> {angka} </p>
+        <button onClick={angkaHandle}>Tambah</button>
+        {angka >= 10 ? <span>State count sudah lebih dari 10!!</span> : ""}
+    </div>
+    );
+}
+
+export default Tugas9
